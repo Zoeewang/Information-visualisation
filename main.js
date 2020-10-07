1d3bfa8c43655f8e8a46b9870396b62a0d5cf510
Zoe wang <Zoe.wang1221@outlook.com>
2020-10-08 09:00:48 +1100

    index

---
+++ main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoiend3YW5nNCIsImEiOiJja2Z5eXF5ZmIwN2d3MnlvMWthNXV0eDY1In0.N-0Dr177HFqwX9_VxWsy_g';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/zwwang4/ckfyytn8d03c219mmf9lsbxky', // stylesheet location
+    center: {lat: -37.814, lng: 144.969}, // starting position [lng, lat]
+    zoom: 13 // starting zoom
+});
+
